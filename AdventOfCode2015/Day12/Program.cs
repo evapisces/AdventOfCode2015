7f2f814ea174ced70b689324b18cf49944ac1df4
Evania <em1419@cs.ship.edu>
2016-12-05 20:09:33 -0500

    Day 12 Part 1 submission.

---
+++ AdventOfCode2015/Day12/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var jsonLines = System.IO.File.ReadAllLines("../../day12input.txt");
+            var sum = 0;
+
+            //ProcessInputA("{\"a\":[-1,1,-3,5]}", ref sum);
+
+            foreach (var json in jsonLines)
+                ProcessInputA(json, ref sum);
+
+            Console.Read();
+        }
+
+        public static void ProcessInputA(string json, ref int sum)
+        {
+            //json = json.Replace("[", "").Replace("]", "").Replace("\"", "");
+            var jSplit = json.Split(new string[] { "[", "]", ":", "\"", "{", "}", "," }, StringSplitOptions.None);
+            var test = Regex.Match(json, @"-?\d+").Value;
+            
+            foreach(var sp in jSplit.Where(s => !string.IsNullOrEmpty(s)))
+            {
+                var num = Regex.Match(sp, @"-?\d+").Value;
+
+                if (!string.IsNullOrEmpty(num))
+                    sum += Int32.Parse(num);
+            }
+
+            Console.WriteLine("Sum = " + sum);
+        }
+    }
+}
