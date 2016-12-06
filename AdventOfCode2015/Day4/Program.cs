649054940991c35fe0b6437cb6d949794c9b6aa3
Evania <em1419@cs.ship.edu>
2016-12-06 12:18:00 -0500

    Adding last year's code for day4.

---
+++ AdventOfCode2015/Day4/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var input = "ckczppom";
+            
+
+            Console.Read();
+        }
+
+        public static void ProcessInputA(string input)
+        {
+            var hash = "";
+            var i = 0; // loop counter;
+            var stop = false;
+
+            using (var md5Hash = MD5.Create())
+            {
+                do
+                {
+                    hash = getMd5Hash(md5Hash, input + i);
+                    if (hash.StartsWith("00000"))
+                    {
+                        Console.WriteLine("Smallest value to produce hash with 5 leading zeros = " + i);
+                        stop = true;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                } while (!stop);
+            }
+        }
+
+        public static void ProcessInputB(string input)
+        {
+            var hash = "";
+            var i = 0; // loop counter;
+            var stop = false;
+
+            using (var md5Hash = MD5.Create())
+            {
+                do
+                {
+                    hash = getMd5Hash(md5Hash, input + i);
+                    if (hash.StartsWith("000000"))
+                    {
+                        Console.WriteLine("Smallest value to produce hash with 6 leading zeros = " + i);
+                        stop = true;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                } while (!stop);
+            }
+        }
+
+        public static string getMd5Hash(MD5 md5Hash, string input)
+        {
+            var data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sb.Append(data[i].ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
