89df5b87804201682d86c59a91ed200754a16870
Evania <em1419@cs.ship.edu>
2016-12-05 19:20:12 -0500

    Code for day 10, parts 1 & 2

--- AdventOfCode2015/Day10/Program.cs
+++ AdventOfCode2015/Day10/Program.cs
@@ -10,40 +10,88 @@ namespace Day10
     {
         public static void Main(string[] args)
         {
-            //var input = "1113122113";
-            var input = "312211";
+            var input = "1113122113";
+            //var input = "1";
+            /*var result = ProcessInputA(input);
 
+            for (var i = 0; i < 40; i++)
+            {
+                result = ProcessInputA(string.Join("", result.ToArray()));
+            }
+
+            Console.WriteLine(string.Join("", result.ToArray()).Length);*/
             ProcessInputA(input);
-
+            ProcessInputB(input);
             Console.Read();
         }
 
         public static void ProcessInputA(string input)
         {
-            var numbersList = new List<string>();
-            string splitList = "";
-            var currNum = input[0];
-            splitList += currNum;
+            for (var i = 0; i < 40; i++)
+            {
+                input = elvesLookElvesSay(input);
+            }
+            Console.WriteLine("Length after 40 iterations = " + input.Length);
+        }
 
-            for (var index = 1; index <= input.Length; index++)
+        public static void ProcessInputB(string input)
+        {
+            for (var i = 0; i < 50; i++)
+            {
+                input = elvesLookElvesSay(input);
+            }
+            Console.WriteLine("Length after 50 iterations = " + input.Length);
+        }
+
+        private static string elvesLookElvesSay(string str)
+        {
+            var result = new StringBuilder();
+
+            var currNum = str[0];
+            str = str.Substring(1, str.Length - 1) + " ";
+            int times = 1;
+
+            foreach(var ch in str)
+            {
+                if (ch != currNum)
+                {
+                    result.Append(Convert.ToString(times) + currNum);
+                    times = 1;
+                    currNum = ch;
+                } else
+                {
+                    times += 1;
+                }
+            }
+
+            return result.ToString();
+        }
+
+        /*public static IEnumerable<int> ProcessInputA(string input)
+        {
+            var currNum = input[0];
+            var ctr = 1;
+            var index = 1;
+
+            while (index < input.Length)
             {
                 if (input[index] == currNum)
                 {
-                    splitList += input[index];
-                    currNum = input[index];
+                    ctr++;
                 }
                 else
                 {
-                    numbersList.Add(splitList);
+                    yield return ctr;
+                    yield return currNum - '0';
                     currNum = input[index];
-                    splitList = input[index].ToString();
-                    if (index == input.Length - 1)
-                    {
-                        numbersList.Add(splitList);
-                        break;
-                    }
+                    ctr = 1;
                 }
+
+                index++;
             }
-        }
+
+            yield return ctr;
+            yield return currNum - '0';
+        }*/
     }
 }
