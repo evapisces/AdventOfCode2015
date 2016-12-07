24c2c9cc34b38ff8b1dad5dc6b81a183245f0ccd
Evania <em1419@cs.ship.edu>
2016-12-06 22:31:02 -0500

    Adding skeleton project for day 16

---
+++ AdventOfCode2015/Day16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day16
+{
+    public class Program
+    {
+        static Dictionary<int, Dictionary<string, int>> sueInfo = new Dictionary<int, Dictionary<string, int>>();
+        public static void Main(string[] args)
+        {
+            var file = File.ReadAllLines("../../day16input.txt");
+
+            ProcessInputA(file);
+
+            sueInfo = new Dictionary<int, Dictionary<string, int>>();
+            ProcessInputB(file);
+
+            Console.Read();
+        }
+
+        public static void ProcessInputA(string[] lines)
+        {
+
+        }
+
+        public static void ProcessInputB(string[] lines)
+        {
+
+        }
+    }
+}
